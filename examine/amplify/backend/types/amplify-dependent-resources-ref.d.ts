3bac51796145a8bf9bcdc2c4488bb8e9088cc992
Abdu Addis <abduaddis@Abdus-MacBook-Pro.local>
2024-02-10 14:53:20 -0500

    Ran amplify add auth

--- examine/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ examine/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -11,6 +11,17 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "examinec3e111a3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "plaidinfoapi": {
       "Arn": "string",
