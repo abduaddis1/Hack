e2458d3bcabc22cfd809877a1fb574118f9146b8
Abdu Addis <abduaddis@Abdus-MacBook-Pro.local>
2024-02-10 00:29:37 -0500

    Added python and nodejs plaid info api

---
+++ examine/amplify/backend/function/plaidinfoapi/src/index.js
@@ -0,0 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	PLAID_CLIENT_ID
+	PLAID_KEY_SANDBOX
+	PLAID_KEY_DEV
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
